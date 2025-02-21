c4cdc721a3e0ee9fe35a8f8a83573cf1efb2fd29
m7he4rt <mateuspm2015@hotmail.com>
2025-02-20 23:08:21 -0300

    fix: layout css

--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="w-full max-w-md flex-1 space-y-4 p-8 pt-6 bg-gradient-to-t from-red-500/5 to-background via-background via-25% rounded-xl shadow-md"
+    class="min-w-screen min-h-screen flex-1 space-y-4 p-8 pt-6 bg-gradient-to-t from-red-500/5 to-background via-background via-25% rounded-xl shadow-md"
   >
     <slot />
   </div>
