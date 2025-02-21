e241428d6eca3ab117860d06b2c0344b7ff2ad75
m7he4rt <mateuspm2015@hotmail.com>
2025-02-21 01:17:03 -0300

    chore: update project dependencies and configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt", "nuxt-svgo"],
+  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt"],
   compatibilityDate: "2024-07-06",
   nitro: {
     devProxy: {
