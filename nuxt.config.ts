65fc0f8499add331bb366d6bd183e82ee542e23c
m7he4rt <mateuspm2015@hotmail.com>
2025-02-21 00:06:12 -0300

    chore: configure API proxy for FruityVice in Nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt"],
+  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt", "nuxt-svgo"],
   compatibilityDate: "2024-07-06",
+  nitro: {
+    devProxy: {
+      "/api": {
+        target: "https://fruityvice.com/api",
+        changeOrigin: true,
+      },
+    },
+  },
 });
